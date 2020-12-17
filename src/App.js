c0792b7202d312fc01e204155ac5d9d2bb106ef1
volidenko <volidenko@.i.ua>
2020-12-17 23:51:16 +0200

    comm

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className="App">
+    // <React.Fragment>
+    <>
+      <h1>Hello React from VS Code</h1>
+  <div>Текущее время: {new Date().toLocaleTimeString()}</div>
+  <CurentDate></CurentDate>
+  <button onClick={clickHandler}>Push me!</button>
+  </>
+  // </React.Fragment>
+    // </div>
   );
 }
 
 export default App;
+
+const style1={
+  border: "3px dotted magenta",
+  backgrondColor:"skyblue",
+  color: 'green',
+  textAlign:'center'
+  // 'text-align':'center'
+}
+
+function CurentDate(){
+  return(
+    <div style={style1}>Сегодня: {new Date().toLocaleDateString()}</div>
+  )
+}
+function clickHandler(){
+  alert("Button was clicked!")
+}
